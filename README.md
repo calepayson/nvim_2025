7147b9560a3468da134e07f812e9404e5bba352f
Cale Smith <calepayson@mac.com>
2024-11-28 13:30:39 -0800

    Add keymap.lua
    - Adds nvim keymappings in keymap.lua.
    - Updated README.md with new keymappings.
    - Point to keymap.lua from init.lua.

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # Neovim Config
 
+## Shortcuts
+
+Format: Keys - result.
+
+**Normal Mode:**
+- " pv" - Open the vim file explorer.
+- "<C-d>" - Scroll down half a page and center the cursor.
+- "<C-u>" - Scroll up half a page and center the cursor.
+- "n" - Center the found item.
+- "N" - Center the found item.
+- " f" - Format the current buffer using the system lsp.
+- " s" - Perform a global search and replace for the word under the cursor.
+
+**Visual Mode:**
+- "J" - Move highlighted line(s) up.
+- "K" - Move highlighted line(s) down.
+- " p" - Paste without overwriting the buffer.
+- " y" - Copy highlighted text to system keyboard.
+- " Y" - Copy line to system keyboard.
+- " d" - Delete the highlighted text without adding it to the buffer.
+
 ## Packages
 
 ### Package manager
---
+++ lua/config/keymap.lua
@@ -0,0 +1,42 @@
+----------------
+-- Keymap.lua --
+----------------
+
+-- Shamelessly stolen from @theprimeagen
+-- Github repo: https://github.com/ThePrimeagen/init.lua/tree/249f3b14cc517202c80c6babd0f9ec548351ec71/lua/theprimeagen
+
+
+-- Set the <leader> key to space
+vim.g.mapleader = " "
+
+-- Add vim file explorer shortcut
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Use 'J' and 'K' to move lines in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+-- Center the cursor after half page scroll
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Center any found items when using search
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Paste without overwriting the buffer
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- Copy to system clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Delete without adding text to the buffer
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- Format the current buffer
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- Global search and replace shortcut
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
