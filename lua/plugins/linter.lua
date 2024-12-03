f642915f4779da3d8f5538408029023f06ecf598
Cale Smith <calepayson@mac.com>
2024-12-03 14:37:56 -0800

    Refactor linter.lua (again)

--- lua/plugins/linter.lua
+++ lua/plugins/linter.lua
@@ -3,18 +3,19 @@ return {
 	-----------------
 	-- Neovim Lint --
 	-----------------
-	-- {
-	-- 	"mfussenegger/nvim-lint",
-	-- 	config = function()
-	-- 		require("lint").linters_by_ft = {
-	-- 			lua = { "luacheck" },
-	-- 		}
+	{
+		"mfussenegger/nvim-lint",
+		config = function()
+			require("lint").linters_by_ft = {
+				-- Example linter
+				-- lua = { "luacheck" },
+			}
 
-	-- 		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-	-- 			callback = function()
-	-- 				require("lint").try_lint()
-	-- 			end,
-	-- 		})
-	-- 	end,
-	-- },
+			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+				callback = function()
+					require("lint").try_lint()
+				end,
+			})
+		end,
+	},
 }
