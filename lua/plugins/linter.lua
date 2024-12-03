c45f3c2278dcd59c0ad09b090df5342fa81bc549
Cale Smith <calepayson@mac.com>
2024-12-03 13:49:13 -0800

    Add linter.lua
    - Adds a plugin for linter configuration.

---
+++ lua/plugins/linter.lua
@@ -0,0 +1,20 @@
+return {
+
+	-----------------
+	-- Neovim Lint --
+	-----------------
+	{
+		"mfussenegger/nvim-lint",
+		config = function()
+			require("lint").linters_by_ft = {
+				lua = { "luacheck" },
+			}
+
+			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+				callback = function()
+					require("lint").try_lint()
+				end,
+			})
+		end,
+	},
+}
