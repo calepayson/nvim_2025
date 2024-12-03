d585b0ab1664bfd5c8d91d147eea0e1b00c711d3
Cale Smith <calepayson@mac.com>
2024-12-03 12:07:13 -0800

    Add lsp.lua
    - Configure an LSP server with Mason.
    - Only has the Lua Language Server installed.

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,61 @@
+return {
+    ----------------
+    -- LSP Config --
+    ----------------
+    {
+        "neovim/nvim-lspconfig",
+    },
+
+    -----------
+    -- Mason --
+    -----------
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup({})
+        end,
+    },
+
+    ----------------------
+    -- Mason LSP Config --
+    ----------------------
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                },
+            })
+
+            -- Define handlers for language servers
+            require("mason-lspconfig").setup_handlers({
+                -- Default handler for all servers
+                function(server_name)
+                    require("lspconfig")[server_name].setup({})
+                end,
+
+                -- Custom configuration for lua_ls
+                ["lua_ls"] = function()
+                    require("lspconfig").lua_ls.setup({
+                        settings = {
+                            Lua = {
+                                diagnostics = {
+                                    -- Recognize `vim` as a global
+                                    globals = { "vim" },
+                                },
+                                workspace = {
+                                    library = {
+                                        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                                        [vim.fn.stdpath("config") .. "/lua"] = true,
+                                    },
+                                },
+                            },
+                        },
+                    })
+                end,
+            })
+        end,
+    },
+}
+
