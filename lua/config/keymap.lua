aaca27eaa62d6f680a2d62eadc9a8c22bd6c64da
Cale Smith <calepayson@mac.com>
2024-12-03 11:02:13 -0800

    Add window navigation keymaps

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -9,6 +9,12 @@
 -- Set the <leader> key to space
 vim.g.mapleader = " "
 
+-- Navigate between windows by holding control and using h, j, k, and l
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
 -- Add vim file explorer shortcut
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
@@ -39,4 +45,3 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 -- Global search and replace shortcut
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-
