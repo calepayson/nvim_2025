54da4d1d61ef23cd2eeec6e4794baf42583d7150
Cale Smith <calepayson@mac.com>
2024-12-03 14:36:20 -0800

    Refactor linter.lua

--- lua/plugins/utilities.lua
+++ lua/plugins/utilities.lua
@@ -1,129 +1,130 @@
 return {
-    ---------------
-    -- Autopairs --
-    ---------------
-    {
-        -- Link the repo
-        "windwp/nvim-autopairs",
-        -- Load when the user enters insert mode
-        event = "InsertEnter",
-        -- Use the default configuration
-        config = true,
-    },
+	---------------
+	-- Autopairs --
+	---------------
+	{
+		-- Link the repo
+		"windwp/nvim-autopairs",
+		-- Load when the user enters insert mode
+		event = "InsertEnter",
+		-- Use the default configuration
+		config = true,
+	},
 
-    ----------------------
-    -- Indent Blankline --
-    ----------------------
-    {
-        -- Link the repo
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        ---@module "ibl"
-        ---@type ibl.config
-        opts = {},
-    },
+	----------------------
+	-- Indent Blankline --
+	----------------------
+	{
+		-- Link the repo
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		---@module "ibl"
+		---@type ibl.config
+		opts = {},
+	},
 
-    ---------------
-    -- Git Signs --
-    ---------------
-    {
-        "lewis6991/gitsigns.nvim",
-        config = function()
-            require("gitsigns").setup({})
-        end,
-    },
+	---------------
+	-- Git Signs --
+	---------------
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({})
+		end,
+	},
 
-    ----------------
-    -- Treesitter --
-    ----------------
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        opts = {
-            ensure_installed = {
-                "bash",
-                "html",
-                "javascript",
-                "json",
-                "lua",
-                "markdown",
-                "markdown_inline",
-                "python",
-                "regex",
-            }
-        }
-    },
+	----------------
+	-- Treesitter --
+	----------------
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"regex",
+			},
+		},
+	},
 
-    -------------
-    -- Lualine --
-    -------------
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("lualine").setup()
-        end,
-    },
+	-------------
+	-- Lualine --
+	-------------
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup()
+		end,
+	},
 
-    --------------
-    -- Undotree --
-    --------------
-    {
-        "mbbill/undotree",
-        config = function()
-            vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
-        end,
-    },
+	--------------
+	-- Undotree --
+	--------------
+	{
+		"mbbill/undotree",
+		config = function()
+			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+		end,
+	},
 
-    ---------------
-    -- Telescope --
-    ---------------
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        config = function()
-            local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-            vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-            vim.keymap.set('n', '<leader>ps', function()
-                builtin.grep_string({ search = vim.fn.input("Grep > ") })
-            end)
-            vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-        end,
-    },
+	---------------
+	-- Telescope --
+	---------------
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+			vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+			vim.keymap.set("n", "<leader>ps", function()
+				builtin.grep_string({ search = vim.fn.input("Grep > ") })
+			end)
+			vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+		end,
+	},
 
-    -------------
-    -- Trouble --
-    -------------
-    {
-        "folke/trouble.nvim",
-        config = function()
-            require("trouble").setup({})
-            vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", {})
-            vim.keymap.set("n", "<leader>xq", "<cmd>Trouble quickfix toggle<cr>", {})
-        end,
-    },
+	-------------
+	-- Trouble --
+	-------------
+	{
+		"folke/trouble.nvim",
+		config = function()
+			require("trouble").setup({})
+			vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", {})
+			vim.keymap.set("n", "<leader>xq", "<cmd>Trouble quickfix toggle<cr>", {})
+		end,
+	},
 
-    -----------
-    -- Mason --
-    -----------
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup({
-                ensure_installed = {
-                    -- LSPs
-                    -- Formatters
-                    "stylua",
-                },
-                ui = {
-                    icons = {
-                        package_installed = "✓",
-                        package_pending = "➜",
-                        package_uninstalled = "✗"
-                    }
-                }
-            })
-        end,
-    },
+	-----------
+	-- Mason --
+	-----------
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({
+				ensure_installed = {
+					-- LSPs
+					-- Formatters
+					"stylua",
+				},
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
+		end,
+	},
 }
