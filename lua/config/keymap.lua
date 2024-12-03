1bc93bf28198602312ecfebda101a420e30a43ab
Cale Smith <calepayson@mac.com>
2024-12-03 10:19:41 -0800

    Fix bug in keymap.lua
    - <leader>K wasn't working properly due to a typo.
    - Fixed the typo.

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -14,7 +14,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Use 'J' and 'K' to move lines in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Center the cursor after half page scroll
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
