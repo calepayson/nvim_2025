31eb4a7fc8f803e947bef47d7fa560d5ad64f971
Cale Smith <calepayson@mac.com>
2024-12-03 15:01:25 -0800

    Add python LSP and formatter
    - LSP: pylsp.
    - Formatter: black.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,52 +1,52 @@
 return {
-    ----------------
-    -- LSP Config --
-    ----------------
-    {
-        "neovim/nvim-lspconfig",
-    },
+	----------------
+	-- LSP Config --
+	----------------
+	{
+		"neovim/nvim-lspconfig",
+	},
 
-    ----------------------
-    -- Mason LSP Config --
-    ----------------------
-    {
-        "williamboman/mason-lspconfig.nvim",
-        dependencies = { "williamboman/mason.nvim" },
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                },
-            })
+	----------------------
+	-- Mason LSP Config --
+	----------------------
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"pylsp",
+				},
+			})
 
-            -- Define handlers for language servers
-            require("mason-lspconfig").setup_handlers({
-                -- Default handler for all servers
-                function(server_name)
-                    require("lspconfig")[server_name].setup({})
-                end,
+			-- Define handlers for language servers
+			require("mason-lspconfig").setup_handlers({
+				-- Default handler for all servers
+				function(server_name)
+					require("lspconfig")[server_name].setup({})
+				end,
 
-                -- Custom configuration for lua_ls
-                ["lua_ls"] = function()
-                    require("lspconfig").lua_ls.setup({
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    -- Recognize `vim` as a global
-                                    globals = { "vim" },
-                                },
-                                workspace = {
-                                    library = {
-                                        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                                        [vim.fn.stdpath("config") .. "/lua"] = true,
-                                    },
-                                },
-                            },
-                        },
-                    })
-                end,
-            })
-        end,
-    },
+				-- Custom configuration for lua_ls
+				["lua_ls"] = function()
+					require("lspconfig").lua_ls.setup({
+						settings = {
+							Lua = {
+								diagnostics = {
+									-- Recognize `vim` as a global
+									globals = { "vim" },
+								},
+								workspace = {
+									library = {
+										[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+										[vim.fn.stdpath("config") .. "/lua"] = true,
+									},
+								},
+							},
+						},
+					})
+				end,
+			})
+		end,
+	},
 }
-
