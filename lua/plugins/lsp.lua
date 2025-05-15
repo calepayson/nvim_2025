cc33f479e30ac69bd4013019c9a10def6c526af4
Cale Smith <calepayson@mac.com>
2025-05-15 09:07:27 -0700

    Modify lsp.lua to prevent an error.
    - Comment out the handlers setup. Pay me.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,32 +23,32 @@ return {
 			})
 
 			-- Define handlers for language servers
-			require("mason-lspconfig").setup_handlers({
-				-- Default handler for all servers
-				function(server_name)
-					require("lspconfig")[server_name].setup({})
-				end,
-
-				-- Custom configuration for lua_ls
-				["lua_ls"] = function()
-					require("lspconfig").lua_ls.setup({
-						settings = {
-							Lua = {
-								diagnostics = {
-									-- Recognize `vim` as a global
-									globals = { "vim" },
-								},
-								workspace = {
-									library = {
-										[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-										[vim.fn.stdpath("config") .. "/lua"] = true,
-									},
-								},
-							},
-						},
-					})
-				end,
-			})
+			-- require("mason-lspconfig").setup_handlers({
+			-- 	-- Default handler for all servers
+			-- 	function(server_name)
+			-- 		require("lspconfig")[server_name].setup({})
+			-- 	end,
+			--
+			-- 	-- Custom configuration for lua_ls
+			-- 	["lua_ls"] = function()
+			-- 		require("lspconfig").lua_ls.setup({
+			-- 			settings = {
+			-- 				Lua = {
+			-- 					diagnostics = {
+			-- 						-- Recognize `vim` as a global
+			-- 						globals = { "vim" },
+			-- 					},
+			-- 					workspace = {
+			-- 						library = {
+			-- 							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+			-- 							[vim.fn.stdpath("config") .. "/lua"] = true,
+			-- 						},
+			-- 					},
+			-- 				},
+			-- 			},
+			-- 		})
+			-- 	end,
+			-- })
 		end,
 	},
 }
